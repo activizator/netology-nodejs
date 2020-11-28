dcab58506c947263ecb011b671f28d9fbfcca0d2
activizator <activizator@ya.ru>
2020-11-28 22:00:54 +0300

    added game logger

--- 4/heads-or-tails.js
+++ 4/heads-or-tails.js
@@ -2,6 +2,7 @@
 
 const readline = require('readline');
 const input = readline.createInterface(process.stdin);
+const fs = require('fs');
 
 class Coin {
     constructor() {
@@ -24,15 +25,52 @@ function guessTheNumber(value) {
     if ( c === 1 || c === 2 ) {
         if (c == coin.side) {
             console.log("Победа!");
-            //process.exit(-1);
+            return "win";
         } else {
             console.log("Победил компьютер");
-            //process.exit(-1);
+            return "lose";
         }
     } else {
         console.log("Нужно ввести 1 или 2");
     }
 }
 
+function syncFileWrite(filename, data) {
+    fs.writeFileSync(filename, data);
+}
+
+function syncFileRead(filename) {
+    let result = fs.readFileSync(filename, 'utf8');
+    return result;
+}
+
+let logFN = process.argv[2];
+let stat;
+if (logFN) {
+    console.log("Логгирование игры ведется в файл: " + logFN);
+    let str;
+    if (fs.existsSync('./' + logFN)) {
+        str = syncFileRead(logFN);
+        stat = JSON.parse(str);
+    } else {
+        stat = {
+            games_ammount: 0,
+            wins_ammount: 0
+        };
+    }
+}
+
 console.log("Угадайте число 1 или 2. Введите вариант:");
-input.on('line', (value) => guessTheNumber(value));
+
+input.on('line', (value) => {
+    let res = guessTheNumber(value);
+
+    if (res && stat) {
+        if (fs.existsSync('./' + logFN)) stat = JSON.parse(syncFileRead(logFN));
+        let data = {
+            games_ammount: stat.games_ammount + 1,
+            wins_ammount: res === "win" ? stat.wins_ammount + 1 : stat.wins_ammount
+        }
+        syncFileWrite(logFN, JSON.stringify(data));
+    }
+});
